8749607c7763b5347859fdd0b5ce75257f94d2af
Fahad Ali <fahadkk833@gmail.com>
2025-07-31 18:18:06 +0500

    Add files via upload

---
+++ Exploring-async.js
@@ -0,0 +1,25 @@
+//async await
+
+console.log("Hello World");
+
+function sayHello(){
+    console.log("Hello Fahad"); 
+}
+
+setTimeout(() => {
+    console.log("Hello Fahad");
+}, 1000);
+
+for(let i = 0; i < 10; i++){
+    console.log(i);
+}
+
+
+
+
+
+
+
+
+
+
